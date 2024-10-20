b026a50902c9f24bd811082536e55077bf8cb0a0
Sabarinathan S <sabarinathans@dlt.sg>
2024-10-20 15:15:00 +0530

    expression constant

--- src/constants.rs
+++ src/constants.rs
@@ -4,6 +4,8 @@ fn main(){
     //const is not mutable
     println!("value of X is {}",X);
     println!("value of Y is {}",Y);
+    println!("Three hours in seconds is {}",THREE_HOURS_IN_SECONDS);
 }
 
 const  X:i32 = 20;
+const THREE_HOURS_IN_SECONDS:u32 = 3*60*60;
