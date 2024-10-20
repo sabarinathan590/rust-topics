ca8cdb1930629ae076f14168753b32082b88e910
Sabarinathan S <sabarinathans@dlt.sg>
2024-10-20 11:57:37 +0530

    main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
 fn main(){
- let mut x = 5;
-//  println!("xp {}",*x);
- let y = &mut x;
- *y +=1;
- 
-*y +=1;
-
- println!("y {}",y);
- 
-
- // can have one mutable reference or n immutable reference
 
 }
