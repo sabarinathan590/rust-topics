98d83f7490aabc4cb70729c702945821b57c5b7d
Sabarinathan S <sabarinathans@dlt.sg>
2024-10-20 15:05:42 +0530

    global constant

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,9 @@
 fn main(){
-    const X:i32 = 10;
+    const Y:i32 = 10;
     // const mut X:i32 = 10;
     //const is not mutable
     println!("value of X is {}",X);
+    println!("value of Y is {}",Y);
 }
+
+const  X:i32 = 20;
